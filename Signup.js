5dfbdd6189f6dcd6a386df5013362ac2d740f9b9
Zaid Khan <zaidkhann0129@gmail.com>
2025-02-20 22:31:36 +0530

    commit

--- Signup.js
+++ Signup.js
@@ -1,11 +1,12 @@
-VANTA.HALO({
-    el: "#cloud",
+VANTA.GLOBE({
+    el: "#your-element-selector",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
     minHeight: 200.00,
-    minWidth: 200.00
-  })
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00})
 
 
 
