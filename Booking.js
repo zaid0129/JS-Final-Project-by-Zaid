fba5eeb09a5389377e2d3a1f595fe5f404f55468
Zaid Khan <zaidkhann0129@gmail.com>
2025-03-20 13:44:54 +0530

    commmit

--- Booking.js
+++ Booking.js
@@ -52,6 +52,29 @@ let paginationData=(data)=>{
     }
     //   fetchData()
     
+    let confirmdelete=(id)=>{
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+              });
+            }
+          });
+
+
+
+    }
     let deletee = (id)=>{
     
         let url = `http://localhost:3000/hotel/${id}`
